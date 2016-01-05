84f320191f48b1576c56902a7e41956c24681776
Jefffrey <tpigarelli@gmail.com>
2016-01-05 10:22:55 +0100

    Trying to fix redirect loop

--- tracy/protected/controllers/SiteController.php
+++ tracy/protected/controllers/SiteController.php
@@ -85,7 +85,7 @@ class SiteController extends Controller
 		}
 
 		// collect user input data
-		if(isset($_POST['LoginForm']))
+		if(isset($_POST['LoginForm']) && !empty($_POST['LoginForm']))
 		{
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
